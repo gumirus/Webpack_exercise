c37842e200b6074ce4c8e97ced58dd0428e057f7
Ruslan Gumirov <gumirus@Mac-Pro-Ruslan.local>
2024-01-21 03:51:37 -0800

    fix(index.js,index.html,style.css)

---
+++ src/app.js
@@ -0,0 +1,9 @@
+// const root = document.querySelector("#app");
+
+// function renderItem(item) {
+//   const li = document.createElement("li");
+//   li.textContent = item.title
+//   root.append(li)
+
+//   app.forEach(renderItem)
+// }
---
+++ src/index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const appTitleElement = document.getElementById("appTitle");
+  appTitleElement.innerText = 'Webpack_exercise (Updated with JavaScript)';
+});
+
